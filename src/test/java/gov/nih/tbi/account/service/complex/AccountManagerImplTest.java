ae8b988e459eb48c9ef0e3462907e276932eb71b
WMW <wnameless@gmail.com>
2014-04-04 09:58:52 -0400

    Fix merge error

--- src/test/java/gov/nih/tbi/account/service/complex/AccountManagerImplTest.java
+++ src/test/java/gov/nih/tbi/account/service/complex/AccountManagerImplTest.java
@@ -23,6 +23,7 @@ package gov.nih.tbi.account.service.complex;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
